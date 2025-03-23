f95e3ef24ac532804b8615da03f1609b6b630bae
GunaPalanivel <gp5901@srmist.edu.in>
2025-03-23 16:57:09 +0530

    Introduced a memory leak issue - gp5901

--- closure_memory.js
+++ closure_memory.js
@@ -1,16 +1,27 @@
+// Feature 1: Closure-based Counter
 function createCounter() {
-    let count = 0;
-    return {
-        increment: function () {
-            count++;
-            console.log("Counter:", count);
-        },
-        decrement: function () {
-            count--;
-            console.log("Counter:", count);
-        },
-        getCount: function () {
-            return count;
-        }
-    };
+  let count = 0;
+  return {
+    increment: function () {
+      count++;
+      console.log("Counter:", count);
+    },
+    decrement: function () {
+      count--;
+      console.log("Counter:", count);
+    },
+    getCount: function () {
+      return count;
+    },
+  };
+}
+
+// Feature 2: Introduced a Memory Leak
+function createLeakyFunction() {
+  let hugeArray = new Array(1000000).fill("leak");
+
+  return function () {
+    console.log("This function is causing a memory leak!");
+    console.log(hugeArray.length);
+  };
 }
