e6473f0e14ba3db337c6c7ebb137a76d7f45fc49
rohithvarma73 <rohithvarma73@gmail.com>
2025-03-24 12:06:03 +0530

    Initial Async Implementation

---
+++ asynchronous.js
@@ -0,0 +1,10 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { user: "Guna", age: 21 };
+      resolve(data);
+    }, 2000);
+  });
+}
+
+fetchData().then((data) => console.log("User Data:", data));
